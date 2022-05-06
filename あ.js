930383aa34e1c90da7c56613ffcfbe4f8866f8de
Takahashi Rena <a8120125@gsuite.si.aoyama.ac.jp>
2022-05-06 15:41:04 +0900

    新しいファイル

---
+++ あ.js
@@ -0,0 +1,7 @@
+var settings = {};
+
+settings.baseurl = 'https://dbkiso.si.aoyama.ac.jp/oursql/';
+settings.apiurl = settings.baseurl + 'api/';
+settings.db = 'isdb';
+settings.userid = 'isp2021';
+settings.password = 'omuice';
